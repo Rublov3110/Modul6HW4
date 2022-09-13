a918736a8a6ba67793427a682ebc389500f780aa
Rublov Viacheslav <timmy31101994@gmail.com>
2022-09-13 21:23:19 +0300

    Module6HW4

---
+++ WebAppMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppMVC.Interfaces;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService) => _employeeService = employeeService;
+
+        public ActionResult Index()
+        {
+            var employees = _employeeService.Get();
+            return View(employees);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            _employeeService.Post(employee);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Employee employee = _employeeService.GetById(id);
+            return View("Create", employee);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            _employeeService.Put(employee);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            _employeeService.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebAppMVC/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace WebAppMVC.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+
+        }
+
+        public Employee(int id, string model, int year, int price)
+        {
+            Id = id;
+            Model = model;
+            Year = year;
+            Price = price;
+        }
+
+        public int Id { get; init; }
+        public string Model { get; init; }
+        public int Year { get; init; }
+        public int Price { get; init; }
+    }
+}
